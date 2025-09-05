2d8a9673017685a0de37062aca29d3cb2fef39de
StarHeartHunt <starheart233@gmail.com>
2025-09-05 14:15:41 +0800

    chore: release 1.29.1

--- torappu/core/utils.py
+++ torappu/core/utils.py
@@ -1,17 +1,13 @@
 from collections.abc import Callable, Coroutine
 from functools import partial, wraps
-from typing import Any, TypeVar
-from typing_extensions import ParamSpec
+from typing import Any
 
 from anyio import from_thread, to_thread
 
 DELIMITERS = r"\. _-"
 
-P = ParamSpec("P")
-R = TypeVar("R")
 
-
-def run_sync(func: Callable[P, R]) -> Callable[P, Coroutine[Any, Any, R]]:
+def run_sync[**P, R](func: Callable[P, R]) -> Callable[P, Coroutine[Any, Any, R]]:
     @wraps(func)
     async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         return await to_thread.run_sync(partial(func, *args, **kwargs))
@@ -19,7 +15,7 @@ def run_sync(func: Callable[P, R]) -> Callable[P, Coroutine[Any, Any, R]]:
     return wrapper
 
 
-def run_async(func: Callable[P, Coroutine[Any, Any, R]]) -> Callable[P, R]:
+def run_async[**P, R](func: Callable[P, Coroutine[Any, Any, R]]) -> Callable[P, R]:
     @wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         return from_thread.run(partial(func, *args, **kwargs))
